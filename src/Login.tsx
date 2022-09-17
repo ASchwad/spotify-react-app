ffe8f09cc22b7ba9c6c790e2deee8d8b8cafd20b
ASchwad <a.schoenenwald@gmx.de>
2022-09-17 19:13:06 +0200

    Outsource Auth logic from Login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,34 +1,7 @@
 import React from 'react';
+import { generateAuthURL } from 'api/spotify';
 import logo from './logo.svg';
 import './Login.css';
-
-function generateRandomString(length: number) {
-  let text = '';
-  const possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-function generateAuthURL() {
-  const client_id = 'fba3cb47d4d14044aeae581cd5be33a1'; // Your client id
-  const redirect_uri = `${process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://aschwad.github.io/spotify-react-app/'}dashboard`; // Your redirect uri
-  const state = generateRandomString(16);
-
-  // localStorage.setItem(stateKey, state);
-  const scope = 'user-read-private user-read-email';
-
-  let url = 'https://accounts.spotify.com/authorize';
-  url += '?response_type=token';
-  url += `&client_id=${encodeURIComponent(client_id)}`;
-  url += `&scope=${encodeURIComponent(scope)}`;
-  url += `&redirect_uri=${encodeURIComponent(redirect_uri)}`;
-  url += `&state=${encodeURIComponent(state)}`;
-  return url;
-}
 
 function Login() {
   return (
--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -1,5 +1,33 @@
 import axios from 'axios';
 
+function generateRandomString(length: number) {
+  let text = '';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+export function generateAuthURL() {
+  const client_id = 'fba3cb47d4d14044aeae581cd5be33a1'; // Your client id
+  const redirect_uri = `${process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://aschwad.github.io/spotify-react-app/'}dashboard`; // Your redirect uri
+  const state = generateRandomString(16);
+
+  // localStorage.setItem(stateKey, state);
+  const scope = 'user-read-private user-read-email';
+
+  let url = 'https://accounts.spotify.com/authorize';
+  url += '?response_type=token';
+  url += `&client_id=${encodeURIComponent(client_id)}`;
+  url += `&scope=${encodeURIComponent(scope)}`;
+  url += `&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  url += `&state=${encodeURIComponent(state)}`;
+  return url;
+}
+
 export function getPlaylists(): Promise<any[]> {
   return new Promise((resolve, reject) => {
     const url = '/me/playlists';
